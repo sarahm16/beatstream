0969c646bb7ad1b1e8f4f5a95cc5641c184f012e
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-09-16 13:48:16 -0700

    axios call in tophits to deezer, console.log results

--- client/src/pages/topHits/topHits.js
+++ client/src/pages/topHits/topHits.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import API from '../../utils/API';
 
 class TopHits extends Component {
+
+    componentDidMount() {
+        axios.get('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
+            .then(res => {
+                console.log(res);
+            })
+    }
 
     render() {
         return(
