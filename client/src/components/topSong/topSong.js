ad2c2628dfa0d6a1f44320a7df99d87f1e167d00
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-09-16 17:07:18 -0700

    moved play button on top of artists image in top hits

--- client/src/components/topSong/topSong.js
+++ client/src/components/topSong/topSong.js
@@ -11,11 +11,19 @@ const TopSong = (props) => {
 
     return(
         <div>
-            <img src={props.track.artist['picture_medium']} /> <br />
-            {props.track.artist.name} <br />
-            {props.track.title} <br />
-            <audio src={props.track.preview} id={props.track.title} /> <br />
-            <button onClick={playSong}>Play</button>
+            <div className='image'>
+                <img src={props.track.artist['picture_medium']} /> <br />
+                <button onClick={playSong} className='play'>
+                    <svg width="10em" height="10em" viewBox="0 0 16 16" className="bi bi-play" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>
+                    </svg>
+                </button>
+            </div>
+            <div className='song-info'>
+                <h4>{props.track.artist.name}</h4>
+                {props.track.title} <br />
+                <audio src={props.track.preview} id={props.track.title} /> <br />
+            </div>
         </div>
     )
 }
