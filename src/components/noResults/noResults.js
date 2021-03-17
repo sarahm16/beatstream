e57fd4eedb9e4fb16edfccd214a5a89db7cb936a
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-03-16 18:24:56 -0700

    added css styling to noresults component

--- src/components/noResults/noResults.js
+++ src/components/noResults/noResults.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.css';
 
 function NoResults() {
     return(
-        <div>
-            No Results
+        <div className='no-results-container'>
+            <h1 className='oops'>Oops!</h1>
+            <div>No results matched your search.</div>
+            {/* <div>Please try a different search or</div> */}
+            <div className='top-hits-link'>
+                <a href='/' style={{ textDecoration: 'none', underline: 'none' }}><i className="fas fa-arrow-left"></i> Top Hits</a>
+            </div>
         </div>
     )
 }
