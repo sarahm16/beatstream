b9f1694b6bb5687d0759456a66f160e7b6b4101d
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-03-26 22:36:03 -0700

    Updates

---
+++ precache-manifest.e11798d53b3607f3b54632cf8efc846e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3814d0c192acef776e95b23cb71f329c",
+    "url": "/beatstream/index.html"
+  },
+  {
+    "revision": "6b39cfec32c1232c7e3c",
+    "url": "/beatstream/static/css/main.fc8062c9.chunk.css"
+  },
+  {
+    "revision": "d53327b03366372578c9",
+    "url": "/beatstream/static/js/2.414f516e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beatstream/static/js/2.414f516e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b39cfec32c1232c7e3c",
+    "url": "/beatstream/static/js/main.5c9ed715.chunk.js"
+  },
+  {
+    "revision": "f9fb3a6702e02cf33fa8",
+    "url": "/beatstream/static/js/runtime-main.4e554c77.js"
+  }
+]);
