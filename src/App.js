a43e96d28acb0713301350314679f9f7b93dbbec
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-04-12 12:12:26 -0700

    removed lazy loading from app.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
-//import Results from './pages/results/results';
+import Results from './pages/results/results';
 import TopHits from './pages/topHits/topHits';
-//import TrackList from './pages/trackList/trackList';
+import TrackList from './pages/trackList/trackList';
 
-const Results = lazy(() => import('./pages/results/results'));
-const TrackList = lazy(() => import('./pages/trackList/trackList'));
+//const Results = lazy(() => import('./pages/results/results'));
+//const TrackList = lazy(() => import('./pages/trackList/trackList'));
 
 function App() {
   return (
     <div>
       
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Suspense fallback={<div>Loading...</div>}>
+      {/* <Suspense fallback={<div>Loading...</div>}> */}
         {/* <Navbar /> */}
         <Route exact path='/results/:artist' component={Results} />
         <Route exact path='/trackList/:album' component={TrackList} />
         <Route exact path='/' component={TopHits} />
-        </Suspense>
+        {/* </Suspense> */}
       </BrowserRouter>
       {/* <Search /> */}
     </div>
