07ef91dc0e6e31142c66bbdf9e95307cd2ea9d6e
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-04-12 12:04:28 -0700

    used react.lazy() to lazy load Results and TrackList components in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
-import Results from './pages/results/results';
+//import Results from './pages/results/results';
 import TopHits from './pages/topHits/topHits';
-import TrackList from './pages/trackList/trackList';
+//import TrackList from './pages/trackList/trackList';
+
+const Results = lazy(() => import('./pages/results/results'));
+const TrackList = lazy(() => import('./pages/trackList/trackList'));
 
 function App() {
   return (
     <div>
+      
       <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Suspense fallback={<div>Loading...</div>}>
         {/* <Navbar /> */}
         <Route exact path='/results/:artist' component={Results} />
         <Route exact path='/trackList/:album' component={TrackList} />
         <Route exact path='/' component={TopHits} />
+        </Suspense>
       </BrowserRouter>
       {/* <Search /> */}
     </div>
